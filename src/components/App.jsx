0af7cfe6dda088bb4352cca384b96bec663c6d1f
Aryan Kalia <kalia.aryan2013@gmail.com>
2021-03-03 23:10:46 -0500

    react-fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,10 @@
 // Libraries
 import React from 'react';
 
-// Components
-// const App = () => (
-//   <div className="header">
-//     <h1>Hello, World</h1>
-//   </div>
-// );
-
 class App extends React.Component {
   render (){
     return (
-      <div className="header">
            <h1>Hello, World</h1>
-      </div>
     )
   }
 }

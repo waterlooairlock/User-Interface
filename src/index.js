15c9e27763c97429996b43b1ca8036b7e8b42444
waterlooairlock <Watlockemail@gmail.com>
2021-03-10 16:00:13 +0000

    Automated Formatting Fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Rendering
 ReactDOM.render(<App />, document.getElementById("root"));
